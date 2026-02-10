3edce667d7fd64a68253f8567681af3a97ce84a1
Darshit Jain <darshitjain09092006@gmail.com>
2026-02-10 12:32:04 +0530

    Final Commit

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+const constants = {
+    port: process.env.PORT || 3000,
+    email: 'darshit1171.be23@chitkarauniversity.edu.in',
+    geminiApiKey: process.env.GEMINI_API_KEY || '',
+};
+
+module.exports = constants;
